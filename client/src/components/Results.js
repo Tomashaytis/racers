27687ba1c8436691544a17dd09c0f0b3a0385ace
pavel <tomashaytis@gmail.com>
2025-04-01 22:44:47 +0400

    С помощью библиотеки react-flip-toolkit реализовал динамическое изменение рейтинга игроков.

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,13 +1,18 @@
 import './Results.css';
+import { Flipper, Flipped } from 'react-flip-toolkit';
 import Result from './Result';
 
 function Results(props) {
     return (
-        <div className="results"> {
-                props.players.map((player) => (
-                    <Result key={player.name} playerColor={player.color} playerName={player.name} playerScore={player.score} />
+        <div className="results"> 
+            <Flipper flipKey={props.players.map(player => player.name).join('')}>    {
+                props.players.sort((player1, player2) => player2.score - player1.score).map((player) => (
+                    <Flipped key={player.name} flipId={player.name}>
+                        <Result playerColor={player.color} playerName={player.name} playerScore={player.score} />
+                    </Flipped>
                 )
             )}
+            </Flipper>
         </div>
     )
 }
